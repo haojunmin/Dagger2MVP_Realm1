8a53b41eefef99784480468beb824a43de77ce62
haojunmin <1149598411@qq.com>
2017-02-23 23:44:57 +0800

    Rejected commit: Default

---
+++ app/src/main/java/com/example/administrator/dagger2mvp_realm/enty/Author.java
@@ -0,0 +1,47 @@
+package com.example.administrator.dagger2mvp_realm.enty;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Author extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+
+    private String name;
+    private String lastname;
+    private RealmList<Book> books;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(final String lastname) {
+        this.lastname = lastname;
+    }
+
+    public RealmList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(final RealmList<Book> books) {
+        this.books = books;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/dagger2mvp_realm/enty/Publisher.java
@@ -0,0 +1,38 @@
+package com.example.administrator.dagger2mvp_realm.enty;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Publisher extends RealmObject {
+
+    @PrimaryKey
+    private int id;
+
+    private String name;
+    private RealmList<Book> books;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public RealmList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(final RealmList<Book> books) {
+        this.books = books;
+    }
+}
