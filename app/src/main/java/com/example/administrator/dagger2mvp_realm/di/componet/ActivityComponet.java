c6ea4832b120ab0a84570a2a191727c6b5c6212e
haojunmin <1149598411@qq.com>
2017-02-23 23:45:57 +0800

    第一次

--- app/src/main/java/com/example/administrator/dagger2mvp_realm/di/componet/ActivityComponet.java
+++ app/src/main/java/com/example/administrator/dagger2mvp_realm/di/componet/ActivityComponet.java
@@ -1,8 +1,20 @@
 package com.example.administrator.dagger2mvp_realm.di.componet;
 
+import android.app.Activity;
+
+import com.example.administrator.dagger2mvp_realm.MainActivity;
+import com.example.administrator.dagger2mvp_realm.di.ActivityScope;
+import com.example.administrator.dagger2mvp_realm.di.module.ActivityModule;
+
+import dagger.Component;
+
 /**
  * Created by Administrator on 2017/2/23.
  */
+@ActivityScope
+@Component(dependencies = AppComponet.class, modules = ActivityModule.class)
+public interface ActivityComponet {
+    Activity getActivity();
 
-public class ActivityComponet {
+    void inject(MainActivity activity);
 }
