0664661037cf54c638fc1faf249c4d84ed60e9f7
haojunmin <1149598411@qq.com>
2017-02-24 10:50:37 +0800

    第一次

--- app/src/main/java/com/example/administrator/dagger2mvp_realm/App.java
+++ app/src/main/java/com/example/administrator/dagger2mvp_realm/App.java
@@ -2,12 +2,20 @@ package com.example.administrator.dagger2mvp_realm;
 
 import android.app.Application;
 
+import com.example.administrator.dagger2mvp_realm.di.componet.AppComponet;
+import com.example.administrator.dagger2mvp_realm.di.componet.DaggerAppComponet;
+import com.example.administrator.dagger2mvp_realm.di.module.AppModule;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by Administrator on 2017/2/23.
  */
 
 public class App extends Application {
     private static App instance;
+    private static AppComponet mAppComponet;
 
     public static synchronized App getInstance() {
         return instance;
@@ -17,5 +25,20 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         instance = this;
+        initRealm();
+    }
+
+    private void initRealm() {
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().name("book.realm").build();
+        Realm.deleteRealm(config);
+        Realm.setDefaultConfiguration(config);
+    }
+
+    public static AppComponet getAppComponet() {
+        if (mAppComponet == null) {
+            mAppComponet = DaggerAppComponet.builder().appModule(new AppModule(instance)).build();
+        }
+        return mAppComponet;
     }
 }
