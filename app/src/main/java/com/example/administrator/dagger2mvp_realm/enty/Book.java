4345380efd88733886dd1aca1a051a9c5970cad3
haojunmin <1149598411@qq.com>
2017-02-24 10:49:34 +0800

    第一次

--- app/src/main/java/com/example/administrator/dagger2mvp_realm/enty/Book.java
+++ app/src/main/java/com/example/administrator/dagger2mvp_realm/enty/Book.java
@@ -5,21 +5,12 @@ import io.realm.annotations.PrimaryKey;
 
 public class Book extends RealmObject {
 
-    @PrimaryKey
-    private int id;
 
     private String isbn;
     private String title;
-    private Author author;
-    private Publisher publisher;
+    // private Author author;
+    // private Publisher publisher;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(final int id) {
-        this.id = id;
-    }
 
     public String getIsbn() {
         return isbn;
@@ -37,42 +28,20 @@ public class Book extends RealmObject {
         this.title = title;
     }
 
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(final Author author) {
-        this.author = author;
-    }
-
-    public Publisher getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(final Publisher publisher) {
-        this.publisher = publisher;
-    }
 
     public String getDetails() {
         StringBuilder details = new StringBuilder();
-        details.append(getAuthorFullName());
+
         details.append(" | ");
-        details.append(getPublisher().getName());
         return details.toString();
     }
 
-    public String getAuthorFullName() {
-        return String.format("%s %s", getAuthor().getName(), getAuthor().getLastname());
-    }
 
     @Override
     public String toString() {
         return "Book{" +
-                "id=" + id +
                 ", isbn='" + isbn + '\'' +
                 ", title='" + title + '\'' +
-                ", author=" + author +
-                ", publisher=" + publisher +
                 '}';
     }
 }
